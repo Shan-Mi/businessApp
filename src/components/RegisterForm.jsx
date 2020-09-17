2c8087e0e65fdbbbfeabaf45da6ff92d2c862b15
Shan <shan.mi.fanfan@gmail.com>
2020-09-17 18:38:52 +0200

    lousy layout

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,5 +1,36 @@
 import React, { useState } from "react";
 import UserKit from "../data/UserKit";
+import styled from "styled-components";
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormContainer = styled.div`
+  text-align: left;
+  margin: auto;
+
+  button {
+    padding: 0.5rem 1rem;
+    display: block;
+    margin: 1.5rem auto;
+    font-size: 1rem;
+  }
+`;
+
+const InputContainer = styled.div`
+  margin: 0.5rem 0;
+  font-size: 1.2rem;
+  label {
+    margin-right: 1rem;
+  }
+  input {
+    height: 1.3rem;
+    font-size: 1rem;
+    width: 200px;
+  }
+`;
 
 const userKit = new UserKit();
 
@@ -22,38 +53,38 @@ const RegisterForm = () => {
 
   function renderInput(index, placeholder, value, setValue) {
     return (
-      <div key={index}>
-        <label htmlFor={placeholder}>{placeholder}</label>
+      <InputContainer key={index}>
+        <label htmlFor={placeholder}>{placeholder}:</label>
         <input
           placeholder={placeholder}
           id={placeholder}
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-      </div>
+      </InputContainer>
     );
   }
 
   function handleRegister() {
-    userKit
-      .register(
-        firstName,
-        lastName,
-        email,
-        password,
-        organisationName,
-        organisationKind
-      )
-      
+    userKit.register(
+      firstName,
+      lastName,
+      email,
+      password,
+      organisationName,
+      organisationKind
+    );
   }
 
   return (
-    <div>
-      {inputItemsArray.map(([placeholder, value, setValue], index) =>
-        renderInput(index, placeholder, value, setValue)
-      )}
-      <button onClick={handleRegister}>Register</button>
-    </div>
+    <FlexContainer>
+      <FormContainer>
+        {inputItemsArray.map(([placeholder, value, setValue], index) =>
+          renderInput(index, placeholder, value, setValue)
+        )}
+        <button onClick={handleRegister}>Register</button>
+      </FormContainer>
+    </FlexContainer>
   );
 };
 
