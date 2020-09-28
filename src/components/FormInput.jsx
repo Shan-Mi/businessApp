bbd9fecd57a60d7685edc41bfc3d65b25bfa2515
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-09-28 21:41:00 +0200

    clean up

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { InputFlexContainer, InputOutsideContainer } from "./FormInput.styles";
 
-// const FormInput = ({ register, error, label, id, ...inputProps }) => {
-//   return (
-//     <InputOutsideContainer>
-//       <InputFlexContainer>
-//         <label htmlFor={id}>{label}</label>
-//         <input ref={register} id={id} {...inputProps} />
-//       </InputFlexContainer>
-//       {error && <p>{error.message}</p>}
-//     </InputOutsideContainer>
-//   );
-// };
-
 const FormInput = ({ value, placeholder, register, errors }) => {
   return (
     <InputOutsideContainer>
       <InputFlexContainer>
         <label htmlFor={value}>{placeholder}</label>
-        <input ref={register} id={value} type={value} name={value} errors={errors[value]} />
+        <input
+          ref={register}
+          id={value}
+          type={value}
+          name={value}
+          errors={errors[value]}
+        />
       </InputFlexContainer>
       {errors[value] && <p>{errors[value].message}</p>}
     </InputOutsideContainer>
