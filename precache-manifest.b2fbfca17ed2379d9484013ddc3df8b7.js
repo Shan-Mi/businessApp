e9a1121d31b1ff86ad74b5b3e86cd4e043ac2240
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-09-29 09:37:42 +0200

    Updates

---
+++ precache-manifest.b2fbfca17ed2379d9484013ddc3df8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8339120dfce68ea180c5ecc3e59c28a8",
+    "url": "/businessApp/index.html"
+  },
+  {
+    "revision": "ff08ecbb266cbde0649e",
+    "url": "/businessApp/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "3eccfa8facb7f990b26d",
+    "url": "/businessApp/static/css/main.ddf2f325.chunk.css"
+  },
+  {
+    "revision": "ff08ecbb266cbde0649e",
+    "url": "/businessApp/static/js/2.59fb171b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/businessApp/static/js/2.59fb171b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eccfa8facb7f990b26d",
+    "url": "/businessApp/static/js/main.98090c2a.chunk.js"
+  },
+  {
+    "revision": "872ee3279469d48458ee",
+    "url": "/businessApp/static/js/runtime-main.359d3eca.js"
+  }
+]);
